0c2ffecd638166f06caa8be37c97ca4c573607c9
Sanu Kumar <sanukumar@Sanus-MacBook-Air.local>
2021-02-06 15:37:50 +0530

    addedpost

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,7 +1,16 @@
+const Post=require('../models/post');
+const post= require('../routes');
+
+
 module.exports.home=function(req,res){
     // return res.end('<p> it is worjing </p>');
-    return res.render('home',{
-        title:'home',
+    // 
+    Post.find({},function(err,posts)
+    {
+        return res.render('home',{
+            title:"Home",
+            posts:posts
+        });
     });
 }
 
